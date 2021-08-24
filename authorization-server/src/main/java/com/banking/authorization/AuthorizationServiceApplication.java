bab083474a2f81654e2c8841dbc1d2b482b92c95
Aaron MAJAMBO <aaronmajb@gmail.com>
2021-08-24 23:18:12 +0200

    Added dependencies for authorization and authentication service

--- authorization-server/src/main/java/com/banking/authorization/AuthorizationServiceApplication.java
+++ authorization-server/src/main/java/com/banking/authorization/AuthorizationServiceApplication.java
@@ -1,2 +1,12 @@
-package com.banking.authorization;public class AuthorizationServiceApplication {
+package com.banking.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationServiceApplication.class,args);
+    }
 }
