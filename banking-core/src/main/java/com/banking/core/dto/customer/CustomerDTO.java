c9bd6d1a3aa8a7020da25ac3d8803cc7e1d51888
Aaron MAJAMBO <aaronmajb@gmail.com>
2021-08-25 01:39:34 +0200

    Changed images types to byte[] in CustomerDTO class

--- banking-core/src/main/java/com/banking/core/dto/customer/CustomerDTO.java
+++ banking-core/src/main/java/com/banking/core/dto/customer/CustomerDTO.java
@@ -23,162 +23,169 @@ public class CustomerDTO {
     private final String occupation;
     private final String phoneNumber;
     private final String email;
-    private final String photo;
-    private final String idPic;
-    private final String contractPic;
+    private byte[] photo;
+    private byte[] idPic;
+    private byte[] contractPic;
     private final String nationality;
-    
 
-    private CustomerDTO(Builder builder){
+
+    private CustomerDTO(Builder builder) {
 
         this.id = builder.id;
-        this.firstName =builder.firstName;
+        this.firstName = builder.firstName;
         this.lastName = builder.lastName;
-        this.customerName =builder.customerName;
+        this.customerName = builder.customerName;
         this.custType = builder.custType;
-        this.dateofBirth =builder.dateofBirth;
+        this.dateofBirth = builder.dateofBirth;
         this.address = builder.address;
-        this.district =builder.district;
+        this.district = builder.district;
         this.city = builder.city;
         this.idNumber = builder.idNumber;
-        this.idType =builder.idType;
+        this.idType = builder.idType;
         this.idDeliveryDate = builder.idDeliveryDate;
-        this.country =builder.country;
+        this.country = builder.country;
         this.gender = builder.gender;
-        this.occupation =builder.occupation;
+        this.occupation = builder.occupation;
         this.phoneNumber = builder.phoneNumber;
-        this.email =builder.email;
+        this.email = builder.email;
         this.photo = builder.photo;
-        this.idPic =builder.idPic;
+        this.idPic = builder.idPic;
         this.contractPic = builder.contractPic;
-        this.nationality =builder.nationality;
+        this.nationality = builder.nationality;
     }
 
-    public static class Builder{
-       private Integer id;
-       private String firstName;
-       private String lastName;
-       private String customerName;
-       private CustType custType;
-       private LocalDate dateofBirth;
-       private String address;
-       private String district;
-       private String city;
-       private String idNumber;
-       private IdType idType;
-       private String country;
-       private String idDeliveryDate;
-       private Gender gender;
-       private String occupation;
-       private String phoneNumber;
-       private String email;
-       private String photo;
-       private String idPic;
-       private String contractPic;
-       private String nationality;
+    public static class Builder {
+        private Integer id;
+        private String firstName;
+        private String lastName;
+        private String customerName;
+        private CustType custType;
+        private LocalDate dateofBirth;
+        private String address;
+        private String district;
+        private String city;
+        private String idNumber;
+        private IdType idType;
+        private String country;
+        private String idDeliveryDate;
+        private Gender gender;
+        private String occupation;
+        private String phoneNumber;
+        private String email;
+        private byte[] photo;
+        private byte[] idPic;
+        private byte[] contractPic;
+        private String nationality;
 
-       public CustomerDTO build(){
-           return new CustomerDTO(this);
-       }
+        public CustomerDTO build() {
+            return new CustomerDTO(this);
+        }
 
-       public Builder withId(Integer id){
-           this.id =id;
-           return this;
-       }
-        public Builder withFirstName(String firstName){
-            this.firstName =firstName;
+        public Builder withId(Integer id) {
+            this.id = id;
             return this;
         }
 
-        public Builder withLastName(String lastName){
-            this.lastName =lastName;
-            return this;
-        }
-        public Builder withCustomerName(String customerName){
-            this.customerName =customerName;
+        public Builder withFirstName(String firstName) {
+            this.firstName = firstName;
             return this;
         }
 
-        public Builder withCustType(CustType custType){
-            this.custType =custType;
+        public Builder withLastName(String lastName) {
+            this.lastName = lastName;
             return this;
         }
 
-        public Builder withDateOfBirth(LocalDate dateofBirth){
-            this.dateofBirth =dateofBirth;
+        public Builder withCustomerName(String customerName) {
+            this.customerName = customerName;
             return this;
         }
 
-        public Builder withAddress(String address){
-            this.address =address;
-            return this;
-        }
-        public Builder withCity(String city){
-            this.city =city;
+        public Builder withCustType(CustType custType) {
+            this.custType = custType;
             return this;
         }
 
-        public Builder withCountry(String country){
-            this.country =country;
+        public Builder withDateOfBirth(LocalDate dateofBirth) {
+            this.dateofBirth = dateofBirth;
             return this;
         }
 
-        public Builder withDistrict(String district){
-            this.district =district;
+        public Builder withAddress(String address) {
+            this.address = address;
             return this;
         }
 
-        public Builder withIdNumber(String idNumber){
-            this.idNumber =idNumber;
-            return this;
-        }
-        public Builder withIdDeliveryDate(String idDeliveryDate){
-            this.idDeliveryDate =idDeliveryDate;
+        public Builder withCity(String city) {
+            this.city = city;
             return this;
         }
 
-        public Builder withOccupation(String occupation){
-            this.occupation =occupation;
+        public Builder withCountry(String country) {
+            this.country = country;
             return this;
         }
 
-        public Builder withIdType(IdType idType){
-            this.idType =idType;
+        public Builder withDistrict(String district) {
+            this.district = district;
             return this;
         }
 
-        public Builder withContractPic(String contractPic){
-            this.contractPic =contractPic;
-            return this;
-        }
-        public Builder withPhoto(String photo){
-            this.photo =photo;
-            return this;
-        }
-        public Builder withPhoneNumber(String phoneNumber){
-            this.phoneNumber =phoneNumber;
+        public Builder withIdNumber(String idNumber) {
+            this.idNumber = idNumber;
             return this;
         }
 
-        public Builder withNationality(String nationality){
-            this.nationality =nationality;
+        public Builder withIdDeliveryDate(String idDeliveryDate) {
+            this.idDeliveryDate = idDeliveryDate;
             return this;
         }
 
-        public Builder withIdPic(String idPic){
-            this.idPic =idPic;
+        public Builder withOccupation(String occupation) {
+            this.occupation = occupation;
             return this;
         }
-        public Builder withGender(Gender gender){
-            this.gender =gender;
+
+        public Builder withIdType(IdType idType) {
+            this.idType = idType;
             return this;
         }
-        public Builder withEmail(String email){
-            this.email =email;
+
+        public Builder withContractPic(byte[] contractPic) {
+            this.contractPic = contractPic;
+            return this;
+        }
+
+        public Builder withPhoto(byte[] photo) {
+            this.photo = photo;
+            return this;
+        }
+
+        public Builder withPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public Builder withNationality(String nationality) {
+            this.nationality = nationality;
+            return this;
+        }
+
+        public Builder withIdPic(byte[] idPic) {
+            this.idPic = idPic;
+            return this;
+        }
+
+        public Builder withGender(Gender gender) {
+            this.gender = gender;
+            return this;
+        }
+
+        public Builder withEmail(String email) {
+            this.email = email;
             return this;
         }
     }
-    
-    
+
 
 }
