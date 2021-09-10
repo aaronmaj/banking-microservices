f05cb1dbce18c3ec3969d1ffb79cc84c3d02febe
Aaron MAJAMBO <aaronmajb@gmail.com>
2021-09-10 12:34:44 +0200

    Commented @Mappings in CustomerMapper interface

--- customer-service/src/main/java/com/banking/customer/mappers/CustomerMapper.java
+++ customer-service/src/main/java/com/banking/customer/mappers/CustomerMapper.java
@@ -5,20 +5,23 @@ import com.banking.customer.model.Customer;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring",
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
+)
 public interface CustomerMapper {
-    @Mappings({
+    /*@Mappings({
             @Mapping(target = "photo", ignore = true),
             @Mapping(target = "idPic", ignore = true),
             @Mapping(target ="contractPic", ignore = true )
-    })
+    })*/
     CustomerDto convertToDto(Customer customer);
-
+    /*
     @Mappings({
             @Mapping(target = "photo", ignore = true),
             @Mapping(target = "idPic", ignore = true),
             @Mapping(target = "contractPic", ignore = true)
-    })
+    })*/
     Customer convertToEntity(CustomerDto customerDto);
 }
