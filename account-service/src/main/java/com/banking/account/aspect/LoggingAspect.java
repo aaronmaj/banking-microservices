59723f25a9a2829076451068fde28ad5d402d692
Aaron MAJAMBO <aaronmajb@gmail.com>
2021-10-01 19:38:24 +0200

    Declaring a @Pointcut for business service

--- account-service/src/main/java/com/banking/account/aspect/LoggingAspect.java
+++ account-service/src/main/java/com/banking/account/aspect/LoggingAspect.java
@@ -1,9 +1,15 @@
 package com.banking.account.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class LoggingAspect {
+
+    @Pointcut("execution(* com.banking.account.service.*.*(..))")
+    private void businessService(){
+
+    }
 }
