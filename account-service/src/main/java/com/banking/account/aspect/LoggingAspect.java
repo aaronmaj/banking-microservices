1559838c4d5cd2fd313d1d898407583e9840af8a
Aaron MAJAMBO <aaronmajb@gmail.com>
2021-10-03 09:56:41 +0200

    Implemented @Before and @AfterReturning advices

--- account-service/src/main/java/com/banking/account/aspect/LoggingAspect.java
+++ account-service/src/main/java/com/banking/account/aspect/LoggingAspect.java
@@ -2,10 +2,16 @@ package com.banking.account.aspect;
 
 import com.banking.account.model.Account;
 import com.banking.account.service.AccountService;
+import com.banking.core.dto.account.AccountDto;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @Aspect
 public class LoggingAspect {
@@ -16,8 +22,19 @@ public class LoggingAspect {
     }
 
     @Pointcut("execution(* com.banking.account.service.*Service+.createAccount(..)) && args(account) && target(accountService)")
-    public void beforeSaving(Account account, AccountService accountService) {
+    public void beforeSaving(AccountDto account, AccountService accountService) {
 
     }
 
+    @Before("com.banking.account.aspect.LoggingAspect.beforeSaving(account,service)")
+    public void doBeforeSave(AccountDto account, AccountService service){
+        log.info("[beforeSave]: ---> Target object {}", service.getClass());
+
+    }
+
+    @AfterReturning(value = "execution(* com.banking.account.service.*Service+.createAccount(..))", returning = "result")
+    public void afterServiceSave(JoinPoint joinPoint, AccountDto result){
+        log.info("[afterServiceSave]: ---> Target object {}",  joinPoint.getTarget().getClass());
+        log.info("[afterServiceSave]: ---> Was account saved? {}", (result != null));
+    }
 }
