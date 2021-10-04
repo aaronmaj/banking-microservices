f48708bd4c358d877e17d9a2ca7920c6266f65aa
Aaron MAJAMBO <aaronmajb@gmail.com>
2021-10-04 09:38:19 +0200

    Fixed Enums rendering in Thymeleaf

--- banking-web/src/main/java/com/banking/web/controller/HomeController.java
+++ banking-web/src/main/java/com/banking/web/controller/HomeController.java
@@ -9,17 +9,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping(value = "/")
 public class HomeController {
 
     private final RestTemplate restTemplate;
 
-    @GetMapping("/customers")
+    @GetMapping("customers")
     public String home(Model model) {
         ParameterizedTypeReference<List<CustomerDto>> responseType =
                 new ParameterizedTypeReference<List<CustomerDto>>() {};
