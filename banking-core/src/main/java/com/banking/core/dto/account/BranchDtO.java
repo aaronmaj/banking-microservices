f34e4ff555ab871b815c5514c19120613f629d47
Aaron MAJAMBO <aaronmajb@gmail.com>
2021-10-04 15:27:22 +0200

    Added @JsonProperty annotations  on BranchDto properties

--- banking-core/src/main/java/com/banking/core/dto/account/BranchDtO.java
+++ banking-core/src/main/java/com/banking/core/dto/account/BranchDtO.java
@@ -1,9 +1,13 @@
 package com.banking.core.dto.account;
 
-public class BranchDtO {
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+public class BranchDtO {
+    @JsonProperty("branch_code")
     private String branchCode;
+    @JsonProperty("branch_name")
     private String branchName;
     private String address;
+    @JsonProperty("contact_number")
     private String contactNumber;
 }
