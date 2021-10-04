e91f82dfc4b8db9a303ef84ea1e103556efee8e4
Aaron MAJAMBO <aaronmajb@gmail.com>
2021-10-04 15:25:12 +0200

    Added @JsonValue on Enum property for proper deserialization

--- banking-core/src/main/java/com/banking/core/dto/account/AccountType.java
+++ banking-core/src/main/java/com/banking/core/dto/account/AccountType.java
@@ -1,8 +1,21 @@
 package com.banking.core.dto.account;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum AccountType {
-    CHECKING,
-    SAVINGS,
-    LOAN,
-    CERTIFICATE_OF_DEPOSIT;
+    CHECKING("Checking"),
+    SAVINGS("Savings"),
+    LOAN("Loan"),
+    CERTIFICATE_OF_DEPOSIT("Certificate of deposit");
+
+    private final String value;
+
+
+    AccountType(String value) {
+        this.value = value;
+    }
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
 }
