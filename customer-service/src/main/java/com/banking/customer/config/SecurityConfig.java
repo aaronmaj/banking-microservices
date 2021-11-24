dc95cfb729c4295759b4a52ffb2fe10098386b8f
Aaron MAJAMBO <aaronmajb@gmail.com>
2021-11-24 10:27:21 +0200

    Did some config changes

--- customer-service/src/main/java/com/banking/customer/config/SecurityConfig.java
+++ customer-service/src/main/java/com/banking/customer/config/SecurityConfig.java
@@ -1,17 +1,12 @@
 package com.banking.customer.config;
-/*
+
 import org.keycloak.adapters.KeycloakConfigResolver;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
-import org.keycloak.adapters.springsecurity.client.KeycloakClientRequestFactory;
-import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -22,12 +17,10 @@ import org.springframework.security.web.authentication.session.RegisterSessionAu
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @Configuration
-//@EnableWebSecurity
-//@EnableGlobalMethodSecurity(jsr250Enabled = true)
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(jsr250Enabled = true)
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 
- */
-public class SecurityConfig {//extends KeycloakWebSecurityConfigurerAdapter {
-/*
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
@@ -54,5 +47,4 @@ public class SecurityConfig {//extends KeycloakWebSecurityConfigurerAdapter {
         return new KeycloakSpringBootConfigResolver();
     }
 
- */
 }
